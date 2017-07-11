b8b8e75378bf73f75002be6f9bcab28a65c324db
Lorenzo Saino <lsaino@fastly.com>
2017-07-11 22:53:13 +0100

    Major revamp of VM setup and upgrade to Ubuntu 16.04

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Such a virtual machine may be used for several reasons, such as:
 It is possible to create this virtual machine in two ways: manually or using [Vagrant](http://www.vagrantup.com).
 
 ## Create a virtual machine manually
-First of all you need to create a new virtual machine with Ubuntu 14.04. To do so, create the virtual machine using your chosen hypervisor (e.g. VirtualBox, VMware Desktop, etc..) and install Ubuntu 14.04 on it.
+First of all you need to create a new virtual machine with Ubuntu 16.04. To do so, create the virtual machine using your chosen hypervisor (e.g. VirtualBox, VMware Desktop, etc..) and install Ubuntu 14.04 on it.
 Once the Ubuntu installation is completed, make sure you have a working Internet connection.
 Then open a terminal and type the following command:
 
@@ -20,35 +20,31 @@ This command will download and execute [this script](https://github.com/fnss/fns
 It will take care of downloading and installing the latest version of FNSS alongside all its dependencies.
 
 ## Create a virtual machine with Vagrant
-You can create a virtual machine more easily using [Vagrant](http://www.vagrantup.com). To do so, you must first download and install Vagrant from its website and ensure you have at least one so called *provider* (i.e. a hypervisor supported by Vagrant) installed, e.g. VirtualBox.
-If this is the first time you use Vagrant or you have never used it to create Ubuntu 14.04 based virtual machines, you need to ensure you have the Ubuntu 14.04 box installed. To do so, open a command shell and type:
+You can create a virtual machine more easily using [Vagrant](http://www.vagrantup.com). To do so, you must first download and install Vagrant from its website and ensure you have at least one Vagrant *provider* (i.e., a hypervisor supported by Vagrant) installed, e.g., VirtualBox.
+If this is the first time you use Vagrant or you have never used it to create Ubuntu 16.04 based virtual machines, you need to ensure you have the Ubuntu 16.04 box installed. To do so, open a command shell and type:
 
-    $ vagrant box add chef/ubuntu-14.04
+    $ vagrant box add bento/ubuntu-16.04
 
-This will download a Ubuntu 14.04 blueprint virtual machine.
+This will download a Ubuntu 16.04 blueprint virtual machine.
 
 Now, from the shell move to the directory where this file is located, which also contains the file `Vagrantfile` and type:
 
     $ vagrant up
 
-This command will create a virtual machine, boot it up and install all required dependencies using the configuration information contained in `Vagrantfile`.
+This command will create a virtual machine, boot it and install all required dependencies using the configuration information contained in `Vagrantfile`.
 
 Now you can SSH to the virtual machine with:
 
     $ vagrant ssh
 
-To shut down the virtual machine simply exit from the SSH session and type:
+To shut down the virtual machine simply exit from the SSH session and run:
 
     $ vagrant halt
 
-Finally, to destroy the virtual machine type:
+Finally, to destroy the virtual machine run:
 
     $ vagrant destroy
 
-## Recommended VM configuration
-This section lists a set of recommended configuration parameters although not all are required:
- * Hypervisor: Oracale VirtualBox 4.3.12
- * Guest OS: Ubuntu 14.04 (Desktop/Server)
- * RAM: 1024+ MB
- * HD: 10+ GB
- * HD format: VMDK (for easier export to OVF format)
+For a complete list of actions available run:
+
+    $ vagrant --help
--- setup.sh
+++ setup.sh
@@ -1,24 +1,8 @@
 #!/bin/sh
 #
-# This script downloads and installs on a clean Ubuntu 14.04 installation everything
-# required to have FNSS operational along with additional useful packages. 
-#
+# This script downloads and installs on a clean Ubuntu 16.04 installation everything
+# required to have FNSS operational along with additional useful packages.
 # This script requires superuser privileges.
-
-# Variables
-FNSS_VERSION='v0.6.1'
-FNSS_REPO_URL='https://www.github.com/fnss/fnss'
-FNSS_DIR=${HOME}/fnss
-FNSS_SRC_DIR=${FNSS_DIR}/fnss-src
-FNSS_DOC_DIR=${FNSS_DIR}/fnss-doc
-FNSS_BIN_DIR=${FNSS_DIR}/fnss-bin
-
-# Notes:
-# A user may want to configure one NAT and one host-only interface so that it can SSH
-# to the VM from the host OS, like it is done with Mininet:
-#  * http://www.brianlinkletter.com/set-up-mininet/
-#  * https://github.com/mininet/openflow-tutorial/wiki/Set-up-Virtual-Machine
-#  * http://mininet.org/vm-setup-notes/
 
 # Set echo
 set -v
@@ -26,73 +10,52 @@ set -v
 # Move to HOME folder
 cd ${HOME}
 
-# Makes sure all packages installed are up-to-date
-sudo apt-get update
-sudo apt-get -y -q upgrade
+# This is to avoid the following warning to show up when running apt-get
+# dpkg-reconfigure: unable to re-open stdin: No file or directory
+export DEBIAN_FRONTEND=noninteractive
 
-# Install generic utilities
-sudo apt-get -y -q install zsh curl mtr traceroute tcptraceroute htop screen vim vim-runtime
+export APT_GET="sudo -E apt-get -y -qq "
+# Make sure all packages installed are up-to-date
+$APT_GET update
 
 # Install Git
-sudo apt-get -y -q install git 
+$APT_GET install git
 
-# Install oh my zsh
-# The shell change may need a reboot to succeed
-curl -L http://install.ohmyz.sh | sh
-
-# Install Python2 utilities and required packages
-sudo apt-get -y -q install python ipython python-pip python-setuptools cython ipython-notebook ipython-qtconsole
-sudo apt-get -y -q install python-dev python-scipy python-numpy python-matplotlib python-networkx python-gnuplot python-mako python-nose
-sudo pip install -U --quiet coverage autonetkit numpydoc cheesecake pylint sphinx topzootools virtualenv virtualenvwrapper
-
-# Install Python3 utilities and required packages
-# Do not install Autonetkit because it has a specific dependency on configobj=4.7.2 which is not compatible with Python 3
-sudo apt-get -y -q install python3 ipython3 python3-pip python3-setuptools cython3 ipython3-notebook ipython3-qtconsole
-sudo apt-get -y -q install python3-dev python3-scipy python3-numpy python3-matplotlib python3-mako python3-nose
-sudo pip3 install -U --quiet networkx coverage numpydoc cheesecake pylint sphinx topzootools virtualenv virtualenvwrapper
-
-# Install FNSS core library
-# Install Py3 version first so that FNSS scripts are executed with Python2 by default
-sudo pip3 install --quiet -U fnss
-sudo pip install --quiet -U fnss
-
-# Install Mininet
-sudo apt-get -y -q install mininet
-sudo service openvswitch-controller stop
-sudo update-rc.d openvswitch-controller disable
-
-# Test Mininet:
-sudo mn --test pingall
+# Install core library requirements
+$APT_GET install python python-pip python-dev
+$APT_GET install libatlas-dev libatlas-base-dev liblapack-dev gfortran libsuitesparse-dev libgdal-dev graphviz mono-devel
 
 # Install Java API requirements
-sudo apt-get -y -q install default-jdk default-jre ant
+$APT_GET install default-jdk default-jre ant
 
 # Install C++ API requirements
-sudo apt-get -y -q install doxygen clang
+$APT_GET install doxygen clang
 
-# Get FNSS source code, build doc and binaries and clean
-mkdir ${FNSS_DIR}
-cd ${FNSS_DIR}
-git clone ${FNSS_REPO_URL} ${FNSS_SRC_DIR}
-cd ${FNSS_SRC_DIR}
-git checkout ${FNSS_VERSION}
-make
-mv ${FNSS_SRC_DIR}/doc ${FNSS_DOC_DIR} 
-mv ${FNSS_SRC_DIR}/dist ${FNSS_BIN_DIR}
-make clean
+# Install Mininet
+# Notes:
+# A user may want to configure one NAT and one host-only interface so that it can SSH
+# to the VM from the host OS, like it is done with Mininet:
+#  * http://www.brianlinkletter.com/set-up-mininet/
+#  * https://github.com/mininet/openflow-tutorial/wiki/Set-up-Virtual-Machine
+#  * http://mininet.org/vm-setup-notes/
+$APT_GET install mininet
 
-# Install ns-3
-# prerequisites
-# sudo apt-get install gcc g++ python python-dev mercurial bzr gdb valgrind gsl-bin \
-#  libgsl0-dev libgsl0ldbl tcpdump flex bison sqlite sqlite3 libsqlite3-dev libxml2 libxml2-dev \ 
-#  libgtk2.0-0 libgtk2.0-dev doxygen graphviz imagemagick \
-#  python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev \ 
-#  libboost-signals-dev libboost-filesystem-dev \
-#  uncrustify vtun lxc \
-#  openmpi-bin openmpi-common openmpi-doc libopenmpi-dev \ 
-#  gcc-multilib
+# Clone all repos
+git clone https://github.com/fnss/fnss.git
+git clone https://github.com/fnss/fnss-java.git
+git clone https://github.com/fnss/fnss-cpp.git
+git clone https://github.com/fnss/fnss-ns3.git
 
-# To install ns-3 follow the instructions at: http://www.nsnam.org/wiki/index.php/Installation
+# Build all repos
+# The -H flag allows pip to cache wheels
+sudo -H make -C fnss install
+make -C fnss-cpp
+sudo make -C fnss-cpp install
+ant -f fnss-java
 
-# Install VirtualBox guest additions
-# sudo apt-het -y -q install virtualbox-guest-additions-iso
+# This script does not install ns-3
+# To install ns-3 and configure it with usage with FNSS, follow these steps:
+# 1. Download or clone ns-3 codebase
+# 2. Copy FNSS ns-3 adapter into ns-3 tree with:
+#    $ make -C fnss-ns3 install NS3_DIR=/path/to/ns3/dir/
+# 3. Build ns-3 following the instructions at: https://www.nsnam.org/wiki/Installation
